0a3dc19c84964937cb9bb62c4b631330880e05c7
I2etr0 <47482322+I2etr0@users.noreply.github.com>
2021-09-08 13:28:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import requests
 from parser import result
 
 
-def yar(text: str):
-    token = "1778066183:AAGZ11m0RmOx-A9lBCnq1ourrzBqm8N3y38"
+def first(text: str):
+    token = "BOT_token"
     url = "https://api.telegram.org/bot"
-    channel_id = ["1093285445", "789730913"]
+    channel_id = "chat_id"
     url += token
     method = url + "/sendMessage"
 
@@ -18,26 +18,27 @@ def yar(text: str):
     if r.status_code != 200:
         raise Exception("post_text error")
 
-def ulya(text: str):
-    token = "1778066183:AAGZ11m0RmOx-A9lBCnq1ourrzBqm8N3y38"
-    url = "https://api.telegram.org/bot"
-    channel_id = "789730913"
-    url += token
-    method = url + "/sendMessage"
+# Send message for second person
+# def second(text: str):
+#     token = "BOT_token"
+#     url = "https://api.telegram.org/bot"
+#     channel_id = "chat_id"
+#     url += token
+#     method = url + "/sendMessage"
 
-    r = requests.post(method, data={
-        "chat_id": channel_id,
-        "text": text
-    })
+#     r = requests.post(method, data={
+#         "chat_id": channel_id,
+#         "text": text
+#     })
 
-    if r.status_code != 200:
-        raise Exception("post_text error")
+#     if r.status_code != 200:
+#         raise Exception("post_text error")
 
 
 def start():
     if __name__ == '__main__':
-        yar(result)
-        ulya(result)
+        first(result)
+        second(result)
         time.sleep(3600)
         start()
 
