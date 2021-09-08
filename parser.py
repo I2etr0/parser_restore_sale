bc7d6b34fa099378ce32d6c844d86e3011afaae5
I2etr0 <47482322+I2etr0@users.noreply.github.com>
2021-09-08 13:29:38 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -15,7 +15,7 @@ for n, i in enumerate(site, 1):
     itemNumber = i.find_next('a', {'class': 'r-discount-table__store-phone'}).text.strip()
     itemPrice = i.find_next('div', {'class': 'r-discount-table__product-price-current'}).text.strip()
     itemDescription = i.find_next('div', {'class': 'r-discount-table__product-description'}).text.strip()
-    res = f'{n}: {itemName} в {itemShop}\nНомер телефона: {itemNumber}\nСтоимость: {itemPrice}\nПродается из-за {itemDescription}.\n\n '
+    res = f'{n}: {itemName} в {itemShop}\nНомер телефона: {itemNumber}\nСтоимость: {itemPrice}\nПричина продажи: {itemDescription}.\n\n '
     mas.extend(res)
 
 result = ''.join(mas) + '\n' + 'URL: https://re-store.ru/discount/'
